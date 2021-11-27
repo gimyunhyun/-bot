5e2efed7fee4c03283644a38e7e420a2c6cda56b
gimyunhyun <95117441+gimyunhyun@users.noreply.github.com>
2021-11-27 15:01:17 +0900

    Update Discord.py

--- Discord.py
+++ Discord.py
@@ -1,7 +1,9 @@
 import discord
+import os
 from discord.ext import commands
 bot = commands.Bot(command_prefix='$')
-TOKEN = 'ODg5NjkyODIyMTY3NTE1MTc2.YUk9Ag.b4E2SYEQpctkzpTWlbD8sMYC1jQ'
+access_token = os.environ["BOT_TOKEN"]
+TOKEN = 'access_token'
 
 @bot.event
 async def on_ready():
