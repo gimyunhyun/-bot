c38c8da2cd40cdab2cc9f38af833864e175fd686
gimyunhyun <95117441+gimyunhyun@users.noreply.github.com>
2021-11-27 16:54:47 +0900

    Update Discord.py

--- Discord.py
+++ Discord.py
@@ -2,7 +2,8 @@ import discord
 import os
 from discord.ext import commands
 bot = commands.Bot(command_prefix='$')
-TOKEN = 'ODg5NjkyODIyMTY3NTE1MTc2.YUk9Ag.b4E2SYEQpctkzpTWlbD8sMYC1jQ'
+access_token = os.environ['BOT_TOKEN']
+TOKEN = 'access_token'
 
 @bot.event
 async def on_ready():
@@ -144,4 +145,4 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
     	await ctx.send("명령어를 찾지 못했습니다.")
 
-bot.run(TOKEN)
+bot.run(access_token)
